c30a233d2c09e85e5172e7366b99342a81bb6a76
booukes <lukaszandrzejpilat@gmail.com>
2025-11-02 11:04:00 +0100

    robots

---
+++ sitemap.mjs
@@ -0,0 +1,19 @@
+import { SitemapStream, streamToPromise } from 'sitemap'
+import { createWriteStream } from 'fs'
+
+const hostname = 'https://antworksnarvia.vercel.app'
+const links = [
+    { url: '/', changefreq: 'daily', priority: 1.0},
+    { url: '/dashboard', changefreq: 'weekly' },
+]
+
+const stream = new SitemapStream({ hostname })
+links.forEach(link => stream.write(link))
+stream.end()
+
+streamToPromise(stream).then(data => {
+    const output = createWriteStream('./public/sitemap.xml')
+    output.write(data)
+    output.end()
+    console.log('✅ Sitemap generated successfully!')
+})
